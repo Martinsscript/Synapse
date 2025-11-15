6e82d8a472f5f68a602e316220cd718c50b24907
Martinscript <rafinhamartins200@gmail.com>
2025-11-15 20:13:24 -0300

    JS da página de login feito.

--- js/login.js
+++ js/login.js
@@ -1,1 +1,21 @@
+const user = document.querySelector('#nomeUsuario')
+const pass = document.querySelector('#senhaUsuario')
+const submit = document.querySelector('#submit')
 
+submit.addEventListener('click', function() {
+    let userSalvo = localStorage.getItem('user')
+    let senhaSalva = localStorage.getItem('senha')
+
+    if (userSalvo === null || senhaSalva === null) {
+        window.alert('Conta criada! Bem-vindo(a), ' + user.value)
+        localStorage.setItem('user', user.value)
+        localStorage.setItem('senha', pass.value)
+        location.reload();
+    } else if (user.value === userSalvo && pass.value === senhaSalva) {
+        location.href = "app.html"
+    } else {
+        window.alert('Usuário ou senha incorretos.')
+        user.value = ''
+        pass.value = ''
+    }
+})
